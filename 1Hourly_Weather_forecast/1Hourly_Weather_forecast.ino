1c6dbd755ffbbad231ef8078e6003a62c8acf31c
alkh6397 <63154614+alkh6397@users.noreply.github.com>
2025-05-20 11:45:18 +0900

    1시간 단위 파싱 및 현재 시간 기준 향후 24시간의 강수량의 총합을 계산
    JSON으로 받아온 API의 float형 rain값을 현재 시간 기준 향후 24시간까지의 총합을 계산한다.
    총합인 rain_sum이 0이면 비 없음, 72 미만이면 약한 비, 360미만 이면 보통비, 그 이상이면 강한 비로 구분하여 PREP값을 각각 0, 1, 2, 3으로 설정
    PREP 값을 아두이노에 넘겨주어 처리한다. 정수 하나만 넘겨주면 되니까 오류 확률 적어진다.
    평균치가 아닌 총합으로 계산하는 이유는 특정 시간 대에 소나기가 쏟아지고 나머지 시간대는 비가 없는 경우
    0 0 0 0 0 0 0 0 0 0 200 0 0 0 0 0 이런식으로 관측될 가능성이 있는데
    평균을 낼 경우 (나머지 많은 0값으로 인해) 소나기가 약한 비로 잘못 해석될 여지가 있으므로
    총합으로 계산하는 것이 맞다.

--- 1Hourly_Weather_forecast/1Hourly_Weather_forecast.ino
+++ 1Hourly_Weather_forecast/1Hourly_Weather_forecast.ino
@@ -3,8 +3,13 @@
 #include <ArduinoJson.h>
 
 // Wi-Fi 정보 입력
-const char* ssid = "wifiID";
-const char* password = "wifiPASSWD";
+const char* ssid = "yourssid";
+const char* password = "yourpasswd";
+
+float rain_sum = 0;//인덱스 8~31까지의 강수량 총합을 저장할 변수
+int rain_code = -1;//사전에 약속된 향후 24시간 비에 대한 코드 정보, 0: 비 없음, 1: 약한 비(24시간 동안 72mm미만(시간당 3mm가 약한 비 이므로 일 당3*24=72mm까지는 약한 비로 볼 수 있음))
+//2: 보통 비 (72이상 360미만)
+//3: 강한 비 (360 이상)
 
 // OpenWeatherMap API
 const char* weatherUrl = "https://pro.openweathermap.org/data/2.5/forecast/hourly?q=Seoul,KR&appid=a8d291fce961ee20987d3d6f961bec58&units=metric";
@@ -55,6 +60,10 @@ void loop() {
         const char* description = forecast["weather"][0]["description"];
         float rain = forecast["rain"]["1h"] | 0.0;
 
+        if(i>=8 && i<=31)
+        {
+          rain_sum+=rain;
+        }
         // 출력
         Serial.println("=== 날씨 예보 ===");
         Serial.print("시간: "); Serial.println(time);
@@ -66,6 +75,35 @@ void loop() {
         Serial.println("=================\n");
         }
         Serial.println("============================================\n");
+        Serial.print("향후 24시간 강수량 총합: ");Serial.println(rain_sum);
+
+        if(rain_sum == 0)
+          rain_code = 0;//비 없음
+        else if(rain_sum<72)
+          rain_code = 1;//약한 비
+        else if(rain_sum<360)
+          rain_code = 2;//보통 비
+        else
+          rain_code = 3;//강한 비
+
+        Serial.print("PREP: ");Serial.print(rain_code);
+        switch(rain_code)
+        {
+          case 0:
+            Serial.println(" ---> 비가 없습니다.");
+            break;
+          case 1:
+            Serial.println(" ---> 약한 비가 올 예정입니다.");
+            break;
+          case 2:
+            Serial.println(" ---> 보통 비가 올 예정입니다.");
+            break;
+          case 3:
+            Serial.println(" ---> 강한 비가 올 예정입니다.");
+            break;
+          default:
+            Serial.println(" ---> RAIN COUNT ERROR!!!");
+        }
         Serial.println("============================================\n");
       }
     } else {
@@ -77,6 +115,11 @@ void loop() {
   } else {
     Serial.println("Wi-Fi 연결이 끊겼습니다.");
   }
-
-  delay(60000);  // 60초 대기
+  // 60초 대기
+  for(int i=60;i>=0;i--)
+  {
+    delay(1000);
+    Serial.print(i);Serial.println("초 후 다음 날씨 호출...");
+  }
+  
 }

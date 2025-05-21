834c029931ad20ace10e0ae38e0e071e6699e1b7
Secuho <secuho@secuho.life>
2025-05-22 00:27:41 +0900

    체크섬 / 크로스체크 알고리즘 적용 통신 코드 (ESP -> Arduino)

---
+++ Checksum_UART_ESP32_TRX/Checksum_UART_ESP32_TRX.ino
@@ -0,0 +1,58 @@
+#define TXD1 19
+#define RXD1 21
+
+HardwareSerial arduSerial(1);
+
+char recent_data, now_data;
+
+void setup() {
+  Serial.begin(115200);
+  arduSerial.begin(115200, SERIAL_8N1, RXD1, TXD1);
+  
+  Serial.println("ESP32 UART Transmitter");
+}
+
+void loop() {
+
+  if (arduSerial.available()) {
+    now_data = arduSerial.read();
+
+    if (now_data == 'f') { //Transmitt Error
+      Serial.println("Transmitt Error.");
+      Serial.println("Try again Trans The data.");
+        for (int i=1; i<=3; i++) {
+          arduSerial.write(recent_data);
+          delay(500);
+        }
+    }
+    
+    else {
+      if (now_data == recent_data) {
+        Serial.println("Communication Complete : ");
+        Serial.print(now_data);
+        arduSerial.write('g'); // g = good
+      }
+
+      else {
+        Serial.println("Checksum Failed.");
+        Serial.println("Sendding Checksum Fail Signal...");
+        arduSerial.write('f');
+        }
+      }
+    }
+
+
+  if (Serial.available()) {
+
+    recent_data = Serial.read();
+
+    for (int i=1; i<=3; i++) {
+    arduSerial.write(recent_data);
+    delay(500);
+    }
+
+  Serial.println("Data Sended : ");
+  Serial.println(recent_data);
+  }
+
+}
---
+++ Checksum_UART_UNO_RXT/Checksum_UART_UNO_RXT.ino
@@ -0,0 +1,72 @@
+#include <SoftwareSerial.h>
+
+SoftwareSerial espSerial(2, 3); // RX, TX
+
+char buffer[3];
+char recent_data;
+int count = 0;
+
+void setup() {
+  Serial.begin(115200);
+  espSerial.begin(115200);
+
+  Serial.println("Arduino UART Receiver");
+}
+
+
+void loop() {
+  if (espSerial.available()) {
+    char incoming = espSerial.read();
+    buffer[count] = incoming;
+    count++;
+
+    if (count == 3) {
+      if (buffer[0] == buffer[1] && buffer[1] == buffer[2]) {
+        recent_data = buffer[0];
+
+        Serial.print("Received: ");
+        Serial.println(recent_data);
+
+        Serial.println("Sending Checksum data...");
+        espSerial.write(recent_data);  // 동일한 값 회신
+
+        unsigned long startTime = millis();
+        while (!espSerial.available()) {
+          if (millis() - startTime > 1000) { // 1초 타임아웃
+            Serial.println("ESP response timeout !");
+            break;
+          }
+        }
+
+        if (espSerial.available()) {
+          char response = espSerial.read();
+          if (response == 'g') {
+            Serial.println("ESP : Checksum Successed.");
+            Serial.println("Communication Complete : ");
+            Serial.print(recent_data);
+          } else if (response == 'f') {
+            Serial.println("ESP : Checksum Failed.");
+            Serial.println("Trying Redata Process...");
+            espSerial.write('f');  // 오류 알림
+          } else {
+            Serial.println("Unexpected response from ESP, SysErr : ");
+            Serial.print(response);
+          }
+        }
+
+      }
+
+      else {
+        Serial.println("Receive Process Error");
+        Serial.println("Trying Redata Process...");
+        espSerial.write('f');  // 오류 알림
+      }
+
+      count = 0; // 버퍼 초기화
+    }
+  }
+
+  if (Serial.available()) {
+    espSerial.write(Serial.read());
+  }
+}

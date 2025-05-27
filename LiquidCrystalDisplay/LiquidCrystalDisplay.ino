f0914c1ef910817a6a92640d5755e62306424197
alkh6397 <63154614+alkh6397@users.noreply.github.com>
2025-05-27 16:03:32 +0900

    LCD 예제

---
+++ LiquidCrystalDisplay/LiquidCrystalDisplay.ino
@@ -0,0 +1,16 @@
+#include <Wire.h>
+#include <LiquidCrystal_I2C.h>
+LiquidCrystal_I2C lcd(0x27, 20, 4);
+
+void setup() {
+  lcd.init();
+  lcd.backlight();
+  lcd.setCursor(0,0);
+  lcd.print("Hello, world!");
+  lcd.setCursor(0,1);
+  lcd.print("I'm Eduino");
+}
+
+void loop() {
+  
+}
